0c60aed93547537b98759fa99f744a600fcbec79
4mser <nicolas.moreno.sagan@gmail.com>
2025-02-23 17:17:29 -0300

    e

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.enableCors({
     origin: ['http://localhost:3001, https://danico-pelis.vercel.app'], 
     methods: ['GET', 'POST', 'PATCH'],
     allowedHeaders: ['Content-Type']
   });
-  const port = process.env.PORT || 3000;
+  
   await app.listen(port);
   console.log(`🚀 Servidor listo en: ${await app.getUrl()}`);
 }
