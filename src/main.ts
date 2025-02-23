f0da7e9c3bc56b26ce50c56d450948cf11b3c48c
4mser <nicolas.moreno.sagan@gmail.com>
2025-02-23 17:25:08 -0300

    cost

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.enableCors({
-    origin: ['http://localhost:3001, https://danico-pelis.vercel.app'], 
-    methods: ['GET', 'POST', 'PATCH'],
-    allowedHeaders: ['Content-Type']
+    origin: [
+      'https://danico-pelis.vercel.app', // Tu dominio en Vercel
+      'http://localhost:3001' // Para desarrollo local
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
   });
   
-  await app.listen(port);
-  console.log(`🚀 Servidor listo en: ${await app.getUrl()}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Servidor listo en: http://0.0.0.0:${port}`);
 }
 bootstrap();
